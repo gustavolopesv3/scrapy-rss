2b021038c2487e9ae8f4329e0e596ae151d902af
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-04-20 21:19:41 -0500

    v1.0

---
+++ scrapy/scrapper.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const RSS = require("rss");
+
+const fetchData = async (url) => {
+  const result = await axios.get(url);
+  return result.data;
+};
+
+const main = async () => {
+  const content = await fetchData("https://ac24horas.com/");
+  const $ = cheerio.load(content);
+  let cards = [];
+
+  $(
+    "div.mvp-widget-feat2-side > .mvp-widget-feat2-side-list > .mvp-feat1-list > a > .mvp-feat1-list-cont > .mvp-feat1-list-text"
+  ).each((i, e) => {
+    const titleCard = $(e).find("span").text().replace(/\s/g, "");
+    const contentCard = $(e).find("h2").text();
+    const data = [{ titleCard, contentCard }];
+    cards.push(data);
+    try {
+      createRss(data);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+main();
+
+function createRss(data) {
+  console.log(data);
+  for (const dados of data) {
+    feed.item({
+      title: dados.titleCard,
+      description: dados.contentCard,
+      date: "agora",
+    });
+  }
+  const xml = feed.xml({ indent: true });
+  fs.writeFileSync("./feed.xml", xml);
+}
+
+const feed = new RSS({
+  title: "Titulo blog",
+  description: "descrição blog",
+  author: "Gustavo Lopes",
+});
