78c135a1170d163420c98060f444311faabd9a96
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-04-21 21:17:26 -0500

    ajuste xml file

--- scrapy/index.js
+++ scrapy/index.js
@@ -8,12 +8,11 @@ router = express.Router();
 setInterval(() => {
   console.log("Nova requisição realizada");
   main();
-}, 30000);
-// router.get("/teste", async (req, res) => {
-//   main();
-//   res.send({
-//     ok: true,
-//   });
+}, 36000);
+
+// router.get("/up", async (req, res) => {
+//   await main();
+//   res.send("ok");
 // });
 
 router.get("/feed", async (req, res) => {
@@ -35,43 +34,44 @@ const main = async () => {
   $(
     "div.mvp-widget-feat2-side > .mvp-widget-feat2-side-list > .mvp-feat1-list > a > .mvp-feat1-list-cont > .mvp-feat1-list-text"
   ).each((i, e) => {
-    const titleCard = $(e).find("span").text().replace(/\s/g, "");
+    const titleCard = $(e).find("span").text().trim();
     const contentCard = $(e).find("h2").text();
 
-    let data = [{ titleCard, contentCard }];
+    let data = { titleCard, contentCard };
     cards.push(data);
-    try {
-      createRss(data);
-      data = [];
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
   });
+  try {
+    createRss(cards);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-function createRss(data) {
+function createRss(cards) {
+  let feed = new RSS({
+    title: "Titulo blog",
+    description: "descrição blog",
+    author: "Gustavo Lopes",
+  });
   let data1 = new Date();
 
   let data2 = new Date(data1.valueOf() - data1.getTimezoneOffset() * 120000);
   var dataBase = data2.toISOString().replace(/\.\d{3}Z$/, "");
 
-  for (const dados of data) {
+  for (const dados of cards) {
     feed.item({
       title: dados.titleCard,
       description: dados.contentCard,
       date: dataBase,
     });
   }
-  const xml = feed.xml({ indent: true });
+  cards = "";
+  let xml = feed.xml({ indent: true });
 
   fs.writeFileSync("./feed.xml", xml);
+  xml = null;
+  feed = null;
+  console.log(xml);
 }
-
-const feed = new RSS({
-  title: "Titulo blog",
-  description: "descrição blog",
-  author: "Gustavo Lopes",
-});
 
 module.exports = router;
