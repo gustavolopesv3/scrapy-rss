3e0fcd5d0532884ed9122321ed248a4b0f91cb2e
Gustavo Lopes <gustavo.lopesv3@gmail.com>
2021-04-22 09:12:09 -0500

    get /

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const port = process.env.PORT || 3333;
 api.use(cors());
 
 api.use('/posts', main);
+api.get('/', async (req, res) => {
+  res.send({
+    Feed,
+  });
+});
 
 app.listen(port, host, function () {
   console.log(`Server started... ${port}`);
