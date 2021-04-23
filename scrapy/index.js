3ed2c47d5e337e51544ed2a37b98266e491b0b16
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-04-22 22:26:34 -0500

    alter page scrapy and add tag image xml

--- scrapy/index.js
+++ scrapy/index.js
@@ -1,23 +1,23 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs');
-const RSS = require('rss');
-const express = require('express');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const RSS = require("rss");
+const express = require("express");
 
 router = express.Router();
 setInterval(() => {
-  console.log('Nova requisição realizada');
+  console.log("Nova requisição realizada");
   main();
 }, 3600000);
 
-// router.get("/up", async (req, res) => {
-//   await main();
-//   res.send("ok");
-// });
+router.get("/up", async (req, res) => {
+  await main();
+  res.send("ok");
+});
 
-router.get('/feed', async (req, res) => {
-  const data = fs.readFileSync('./feed.xml');
-  res.set('Content-Type', 'text/xml');
+router.get("/feed", async (req, res) => {
+  const data = fs.readFileSync("./feed.xml");
+  res.set("Content-Type", "text/xml");
   return res.send(data);
 });
 
@@ -27,17 +27,19 @@ const fetchData = async (url) => {
 };
 
 const main = async () => {
-  const content = await fetchData('https://ac24horas.com/');
+  const content = await fetchData("https://ac24horas.com/ultimas-noticias/");
   const $ = cheerio.load(content);
   let cards = [];
 
-  $(
-    'div.mvp-widget-feat2-side > .mvp-widget-feat2-side-list > .mvp-feat1-list > a > .mvp-feat1-list-cont > .mvp-feat1-list-text'
-  ).each((i, e) => {
-    const titleCard = $(e).find('span').text().trim();
-    const contentCard = $(e).find('h2').text();
+  $("ul.mvp-blog-story-list >").each((i, e) => {
+    const titleCard = $(e).find("h2").text().trim();
+    const contentCard = $(e).find("p").text().trim();
+    const linkCard = $(e).find("li > a").attr("href");
+    const imgCard = $(e)
+      .find("li > a > div.mvp-blog-story-out > div.mvp-blog-story-img > img")
+      .attr("src");
 
-    let data = { titleCard, contentCard };
+    let data = { titleCard, contentCard, linkCard, imgCard };
     cards.push(data);
   });
   try {
@@ -49,26 +51,28 @@ const main = async () => {
 
 function createRss(cards) {
   let feed = new RSS({
-    title: 'Titulo blog',
-    description: 'descrição blog',
+    title: "Titulo blog",
+    description: "descrição blog",
     //author: 'Gustavo Lopes',
   });
   let data1 = new Date();
 
   let data2 = new Date(data1.valueOf() - data1.getTimezoneOffset() * 120000);
-  var dataBase = data2.toISOString().replace(/\.\d{3}Z$/, '');
+  var dataBase = data2.toISOString().replace(/\.\d{3}Z$/, "");
 
   for (const dados of cards) {
     feed.item({
       title: dados.titleCard,
       description: dados.contentCard,
       date: dataBase,
+      url: dados.linkCard,
+      custom_elements: [{ image: dados.imgCard }],
     });
   }
-  cards = '';
+  cards = "";
   let xml = feed.xml({ indent: true });
 
-  fs.writeFileSync('./feed.xml', xml);
+  fs.writeFileSync("./feed.xml", xml);
   xml = null;
   feed = null;
   console.log(xml);
